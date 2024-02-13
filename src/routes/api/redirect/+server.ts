9da3291dfb4db126cb0b0dc9ca216809ad1fa269
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2024-02-13 18:09:55 +0530

    Update +server.ts

--- src/routes/api/redirect/+server.ts
+++ src/routes/api/redirect/+server.ts
@@ -2,9 +2,9 @@ import { json, type RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({ params, platform }) => {
   const db = platform?.env.DB;
-  const results = await db.prepare("SELECT * FROM user WHERE id = ?1", [
-    "47h00akws3yf7pu",
-  ]);
+  const results = await db.prepare(
+    "SELECT * FROM user WHERE id = '47h00akws3yf7pu'",
+  );
 
-  return json({ results: results });
+  return json({ results: results.results });
 };
